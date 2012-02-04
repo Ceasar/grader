8803f73cc8a017e1ee263c528261f2a1abd79ad4
Ceasar Bautista <cbautista2010@gmail.com>
2012-02-03 19:53:50 -0500

    Added example tests, and updated grader to lazily run tests, so as to ensure any hanging test don't block the tester from prininting results in a sensible fashion.

--- grader.py
+++ grader.py
@@ -14,36 +14,30 @@ class Problem(object):
   def max_grade(self):
     return sum(self.weights.values())
 
-  @property
-  def results(self):
-    if self._results:
-      return self._results
-    else:
-      for test_name, _ in self.weights.iteritems():
-        test = self._get_test_from_test_name(test_name)
-        result = unittest.TestResult()
-        test.run(result)
-        self._results[test_name] = result
-      return self._results
+  def _get_test_from_test_name(self, test_name):
+    return self._test_case(test_name)
+
+  def result(self, test_name):
+    try:
+      return self._results[test_name]
+    except KeyError:
+      test = self._get_test_from_test_name(test_name)
+      result = unittest.TestResult()
+      test.run(result)
+      self._results[test_name] = result
+      return result
 
   @property
   def grade(self):
-    final_score = 0
-    for test_name, weight in self.weights.iteritems():
-      result = self.results[test_name]
-      if result.wasSuccessful():
-        final_score += weight
-    return final_score
-
-  def _get_test_from_test_name(self, test_name):
-    return self._test_case(test_name)
+    return sum(weight for test_name, weight in self.weights.iteritems() \
+        if self.result(test_name).wasSuccessful())
 
   def print_results(self):
     """Print the results of the tests and the grade for the problem."""
     print "Grading %s..." % self._test_case
     for test_name, weight in self.weights.iteritems():
       print "Running %s..." % test_name
-      result = self.results[test_name]
+      result = self.result(test_name)
       if result.wasSuccessful():
         print "Points: %d/%d" % (weight, weight)
       else:
@@ -72,7 +66,7 @@ class Grader(object):
     print "=" * 70
     total, max_pt = 0, 0
     for problem in self.problems:
+      problem.print_results()
       total += problem.grade
       max_pt += problem.max_grade
-      problem.print_results()
     print "Final Grade: %d/%d" % (total, max_pt)
